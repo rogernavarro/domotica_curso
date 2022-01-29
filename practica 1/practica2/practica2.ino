065561df87dbbbc7a5335e85e55354a5e8ca55ab
Roger Navarro <roger.navarro.perez@gmail.com>
2022-01-29 11:43:51 -0600

    ejemplo 4

--- practica 1/practica2/practica2.ino
+++ practica 1/practica2/practica2.ino
@@ -13,16 +13,20 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int valorpot = analogRead(A0);
+  float vol = valorpot * (255.0/1023.0);
 
   Serial.print(valorpot);
+  Serial.print(" ");
+  Serial.print(vol);
+  Serial.print(" ");
   
-  analogWrite( led1, valorpot );  //MANDAMOS QUE SE PRENDA EL LED
+  analogWrite( led1, vol );  //MANDAMOS QUE SE PRENDA EL LED
   delay(1000);
   analogWrite( led1, 0 );         //MANDAMOS SE APAGA EL LED
-  analogWrite( led2, valorpot );  //MANDAMOS QUE SE PRENDA EL LED
+  analogWrite( led2, vol );  //MANDAMOS QUE SE PRENDA EL LED
   delay(1000);
   analogWrite( led2, 0 );         //MANDAMOS SE APAGA EL LED
-  analogWrite( led3, valorpot );  //MANDAMOS QUE SE PRENDA EL LED
+  analogWrite( led3, vol );  //MANDAMOS QUE SE PRENDA EL LED
   delay(1000);
   analogWrite( led3, 0 );         //MANDAMOS SE APAGA EL LED
 }
